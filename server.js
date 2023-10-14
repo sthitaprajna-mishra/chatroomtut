1a87a7be08062563b792dcca9b7e49c9bd9dfd46
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-14 12:08:18 +0530

    handle creating rooms

--- server.js
+++ server.js
@@ -14,11 +14,90 @@ app.use(
   })
 );
 
+const rooms = {};
+const users = {}; // Dictionary to store user information and their rooms
+
+function generateUniqueRoomCode() {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const codeLength = 6; // Adjust the length of the room code as needed
+
+  let roomCode;
+  let isUnique = false;
+
+  // Keep generating room codes until a unique one is found
+  while (!isUnique) {
+    roomCode = "";
+
+    for (let i = 0; i < codeLength; i++) {
+      const randomIndex = Math.floor(Math.random() * characters.length);
+      roomCode += characters.charAt(randomIndex);
+    }
+
+    // Check if the generated code is unique
+    if (!rooms[roomCode]) {
+      isUnique = true;
+    }
+  }
+
+  return roomCode;
+}
+
+function handleRoomCleanup(socketId) {
+  // remove user from all rooms
+  Object.entries(rooms).forEach(([key, value]) => {
+    if (value["members"].includes(socketId)) {
+      value["members"] = value["members"].filter(
+        (memberId) => memberId !== socketId
+      );
+    }
+  });
+
+  // if user was owner of any room, take action
+  Object.entries(rooms).forEach(([key, value]) => {
+    if (value["owner"] === socketId) {
+      // if more users present, make next user as owner
+      if (value["members"].length > 0) {
+        value["owner"] = value["members"][0];
+      }
+      // if no users present, remove room
+      delete rooms[key];
+    }
+  });
+}
+
 // run when client connects
 io.on("connection", (socket) => {
   console.log("New WS Connection...");
 
+  // create room
+  socket.on("createRoom", () => {
+    // generate a unique room code
+    const roomCode = generateUniqueRoomCode();
+
+    // create a new room with the owner (current socket)
+    rooms[roomCode] = { owner: socket.id, members: [socket.id] };
+
+    // join room as owner
+    socket.join(roomCode);
+
+    // Emit an event to the owner client to inform them about the room creation
+    socket.emit("roomCreated", roomCode);
+
+    console.log(rooms);
+  });
+
+  // welcome current user
   socket.emit("message", "Welcome to Chatroom");
+
+  // broadcast when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+
+  // runs when client disconnects
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} has left the chat`);
+    handleRoomCleanup(socket.id);
+    io.emit("message", "A user has left the chat");
+  });
 });
 
 const PORT = 3000 || process.env.PORT;
