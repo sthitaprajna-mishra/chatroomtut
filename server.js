57a0ba20c55f7ab470b1aef2c6be74ba87e61549
sthitaprajna-mishra <sthitaprajna360@gmail.com>
2023-10-15 00:41:49 +0530

    backend logic for multiple users

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(
   })
 );
 
+const allSockets = {};
 const rooms = {};
 const users = {}; // Dictionary to store user information and their rooms
 
@@ -43,6 +44,11 @@ function generateUniqueRoomCode() {
 }
 
 function handleRoomCleanup(socketId) {
+  // remvoe user from list
+  Object.entries(users).forEach(([key, value]) => {
+    if (key === socketId) delete users[key];
+  });
+
   // remove user from all rooms
   Object.entries(rooms).forEach(([key, value]) => {
     if (value["members"].includes(socketId)) {
@@ -69,13 +75,23 @@ function handleRoomCleanup(socketId) {
 io.on("connection", (socket) => {
   console.log("New WS Connection...");
 
+  console.log("-----ROOMS------");
+  console.log(rooms);
+  console.log("----USERS-------");
+  console.log(users);
+
   // create room
   socket.on("createRoom", (ownerName) => {
     // generate a unique room code
     const roomCode = generateUniqueRoomCode();
 
     // add owner data to user list
-    users[socket.id] = ownerName;
+    users[socket.id] = {
+      userId: socket.id,
+      userName: ownerName,
+      ownerRoom: [roomCode],
+      memberRoom: [roomCode],
+    };
 
     // create a new room with the owner (current socket)
     rooms[roomCode] = { owner: socket.id, members: [socket.id] };
@@ -83,19 +99,77 @@ io.on("connection", (socket) => {
     // join room as owner
     socket.join(roomCode);
 
+    allSockets[roomCode] = [];
+    // store all sockets per room
+    allSockets[roomCode].push(socket);
+
     // send roomCode + ownerName obj back
     const result = {
       roomCode: roomCode,
-      userData: {
-        userId: socket.id,
-        userName: users[socket.id],
-      },
+      userData: users[socket.id],
     };
 
     // Emit an event to the owner client to inform them about the room creation
     socket.emit("roomCreated", result);
 
-    console.log(rooms);
+    // console.log(rooms);
+  });
+
+  // join room
+  socket.on("joinRoom", (joineeName, roomCodeInput) => {
+    // if roomCode does not exist, return
+    if (!(roomCodeInput in rooms)) return;
+
+    // add user data to user list
+    users[socket.id] = {
+      userId: socket.id,
+      userName: joineeName,
+      ownerRoom: [],
+      memberRoom: [roomCodeInput],
+    };
+
+    // join user to room
+    rooms[roomCodeInput]["members"].push(socket.id);
+
+    // store all sockets per room
+    allSockets[roomCodeInput].push(socket);
+
+    const userList = [];
+
+    Object.entries(users).forEach(([key, value]) => {
+      if (value["memberRoom"].includes(roomCodeInput)) {
+        userList.push(value);
+      }
+    });
+
+    // send roomCode + userList back
+    const result = {
+      roomCode: roomCodeInput,
+      userList,
+    };
+
+    // join room as user
+    socket.join(roomCodeInput);
+
+    // const roomSockets = allSockets[roomCodeInput];
+
+    // console.log("------ALL SOCKETS-----");
+    // console.log(allSockets);
+
+    // if (roomSockets) {
+    //   roomSockets.forEach((client) => {
+    //     client.emit("roomJoined", result);
+    //   });
+    // }
+
+    // emit an event to everyone in same room
+    // io.to(roomCodeInput).emit("roomJoined", result);
+    io.emit("roomJoined", result);
+
+    // Emit an event to the owner client to inform them about the room creation
+    // socket.emit("roomJoined", result);
+
+    // console.log(rooms);
   });
 
   // welcome current user
